dcb349433689e9898bf4ce4e770ed142369bc255
danielgynn <danielgynn@hotmail.co.uk>
2017-04-15 21:38:36 +0100

    fixing a hole 🕳

--- src/sketches/sandsplines.js
+++ src/sketches/sandsplines.js
@@ -2,32 +2,27 @@ function setup() {
   noLoop();
   noFill();
   colorMode(HSB);
-
   createCanvas(800, 500);
   smooth(2);
   pixelDensity(displayDensity());
-
-  background(95);
-  // blendMode(MULTIPLY);
-
-  // Slightly inset the drawing
-  translate(width*0.1, height*0.1);
+  background(100);
+  translate(width*0.05, -150);
 
   // set the increment - this changes how many lines are drawn
-  inc = u(4);
+  inc = u(1.5);
 
   for (i = 0; i < height * 1; i += inc) {
     // set an initial volatility
-    vol = random(0.5);
+    vol = random(0.6);
     layers = 8; // the number of times each line is drawn
     // use a different color each render
     h = random(0, 255);
 
     push();
-    translate(0, i+1);
-    for (j = 0; j < layers; j++) {
+    translate(0, i-10);
+    for (j = 0; j < 8; j++) {
       t = map(j, 1, layers, 4, 60);
-      stroke(h, 200, 200, t); // change to h, 200, 200, t
+      stroke(h, 50, 100, t); // change to h, 200, 200, t
       drawLine(0, width*0.8, vol);
     }
     pop();
@@ -49,13 +44,13 @@ function drawLine(x1, x2, volatility) {
   beginShape();
   curveVertex(0, x1);
   for (i = 1; i < granularity; i++) {
-    y = map(noise(yoff), 0, 1, -u(40), u(40)) * (volatility*noise(i/500));
-    x = (w/granularity)*i;
+    y = map(noise(yoff), 0, 1, -u(30), u(30)) * (volatility * noise(i / 150));
+    x = (w / granularity) * i;
     x += map(noise(xoff), 0, 1, -u(20), u(20));
-    y *= exp(i*0.01)*0.015;
+    y *= exp(i * 0.01) * 0.005;
     curveVertex(x, y);
-    yoff += 0.2;
-    xoff += 0.015;
+    yoff += 0.1;
+    xoff += 0.01;
   }
   endShape();
 }
@@ -63,9 +58,5 @@ function drawLine(x1, x2, volatility) {
 function keyPressed() {
   if (keyCode === ENTER) {
     saveCanvas('sandsplines', 'jpg');
-  } else if (keyCode === CONTROL) {
-    // translate(width*0.1, height*0.5);
-    vol = random(0.5);
-    drawLine(0, width*0.6, vol);
   }
 }
